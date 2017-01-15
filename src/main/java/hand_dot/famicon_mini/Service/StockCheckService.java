19d3ac603ec99967d484221d1be7827d33a9de51
hand-dot <kyouhei.fukuda0729@gmail.com>
2017-01-16 01:03:26 +0900

    在庫チェッククラスを修正

--- src/main/java/hand_dot/famicon_mini/Service/StockCheckService.java
+++ src/main/java/hand_dot/famicon_mini/Service/StockCheckService.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import hand_dot.famicon_mini.Enums.CheckType;
 import hand_dot.famicon_mini.Enums.SiteType;
 import hand_dot.famicon_mini.Exceptions.StockCheckException;
 
@@ -25,21 +26,14 @@ public class StockCheckService {
 	/** アクセスするサイトの情報 */
 	private SiteType siteType;
 
-	/** ユーザーエージェント*/
+	/** ユーザーエージェント */
 	private final String UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36";
 
+	public StockCheckService() {
+	}
+
 	public StockCheckService(String url) {
-		try {
-			this.url = url;
-			this.siteType =divideSiteType(url);
-			this.document = Jsoup.connect(url)
-					.userAgent(UA)
-					.maxBodySize(0).get();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}catch (StockCheckException e) {
-			e.printStackTrace();
-		}
+		setUrl(url);
 	}
 
 	/**
@@ -49,44 +43,64 @@ public class StockCheckService {
 	 * @throws StockCheckException
 	 */
 	public boolean check() throws StockCheckException {
-		//サイトのhtmlを解析してIDで絞り込んだテキスト
-		String str = this.getTextById();
+		// サイトのhtmlを解析してIDで絞り込んだテキスト
+		String str = null;
+		if(siteType.getCheckType() == CheckType.text){
+			str  = this.getTextByselect();
+		}else if(siteType.getCheckType() == CheckType.html){
+			str  = this.getHtmlByselect();
+		}
 		Boolean bool = null;
-		if(isMatch(str, this.siteType.getInStockWord())){
+		if (isMatch(str, this.siteType.getInStockWord())) {
 			bool = true;
-		}else if(isMatch(str,this.siteType.getOutStockWord())){
+		} else if (isMatch(str, this.siteType.getOutStockWord())) {
 			bool = false;
-		}else{
-			throw new StockCheckException("在庫が存在するもしくは存在しないのどちらかに当てはまりませんでした。"+""+"/解析テキスト:"+str);
+		} else {
+			throw new StockCheckException("在庫チェックの解析結果が異常です。\n" + this.siteType.toString() + "\n解析テキスト:" + str);
 		}
 		return bool;
 	}
 
 	/**
-	 *URLからサイト情報の設定を行います。
-	 *どのサイトにも当てはまらない場合は例外を投げます。
+	 * URLからサイト情報の設定を行います。 どのサイトにも当てはまらない場合は例外を投げます。
+	 *
 	 * @param url
 	 * @return
 	 * @throws StockCheckException
 	 */
 	private SiteType divideSiteType(String url) throws StockCheckException {
-		SiteType siteType=null;
-		if (this.isMatch(url, SiteType.amazon.getDomain())) {
-			siteType = SiteType.amazon;
+		SiteType siteType = null;
+		for (SiteType site : SiteType.values()) {
+			if (this.isMatch(url, site.getDomain())) {
+				siteType = site;
+			}
 		}
-		if(siteType==null){
-			throw new StockCheckException("アクセスするURLはどのサイトタイプにも当てはまりません/URL:"+url);
+		if (siteType == null) {
+			throw new StockCheckException("アクセスするURLはどのサイトタイプにも当てはまりません/URL:" + url);
 		}
 		return siteType;
 	}
 
 	/**
-	 * アクセスしたページ内の文字列をサイト情報のIdで絞り込んで取得する。
+	 * アクセスしたページ内の文字列をサイト情報のクエリで絞り込んで取得する。
 	 *
 	 * @return テキスト部分だけを抽出した文字列
 	 */
-	private String getTextById() {
-		return document.getElementById(this.siteType.getElemId()).text();
+	private String getTextByselect() {
+		String string = document.select(this.siteType.getQuery()).text();
+		System.out.println(string);
+		return string;
+	}
+
+	/**
+	 * アクセスしたページ内のHTMLをサイト情報のクエリで絞り込んで取得する。
+	 *
+	 * @return テキスト部分だけを抽出した文字列
+	 */
+	private String getHtmlByselect() {
+		String string = document.select(this.siteType.getQuery()).outerHtml();
+		System.out.println(string);
+		return string;
 	}
 
 	/**
@@ -109,6 +123,22 @@ public class StockCheckService {
 	}
 
 	public void setUrl(String url) {
-		this.url = url;
+		try {
+			this.url = url;
+			this.siteType = divideSiteType(url);
+			this.document = Jsoup.connect(url).userAgent(UA).maxBodySize(0).get();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (StockCheckException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public SiteType getSiteType() {
+		return siteType;
+	}
+
+	public void setSiteType(SiteType siteType) {
+		this.siteType = siteType;
 	}
 }
