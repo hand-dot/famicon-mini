9428dac33a619024d1d76c61e9253564e190bf5b
hand-dot <kyouhei.fukuda0729@gmail.com>
2017-01-17 01:27:07 +0900

    TODOの追加

--- src/main/java/hand_dot/famicon_mini/Facade/StockCheckFacade.java
+++ src/main/java/hand_dot/famicon_mini/Facade/StockCheckFacade.java
@@ -29,12 +29,16 @@ public class StockCheckFacade {
 	 MailService mailService;
 
 
+	 /**
+	  * 10秒間隔でサイトをチェックし在庫があればメールを送信します。
+	  */
+	 //TODO 実行時のログをとるようにする
 	@Scheduled(fixedRate = 10000)
 	public void stockCheck() {
 		/** ロガー */
 		Logger logger = Logger.getLogger("operation");
 		PropertyConfigurator.configure("log4j.properties");
-
+		//TODO DBから取得する
 		String amazon = "https://www.amazon.co.jp/%E4%BB%BB%E5%A4%A9%E5%A0%82-CLV-S-HVCC-%E3%83%8B%E3%83%B3%E3%83%86%E3%83%B3%E3%83%89%E3%83%BC%E3%82%AF%E3%83%A9%E3%82%B7%E3%83%83%E3%82%AF%E3%83%9F%E3%83%8B-%E3%83%95%E3%82%A1%E3%83%9F%E3%83%AA%E3%83%BC%E3%82%B3%E3%83%B3%E3%83%94%E3%83%A5%E3%83%BC%E3%82%BF/dp/B01M1VMFUA/";
 		String amazon2 = "https://www.amazon.co.jp/%E3%83%8B%E3%83%B3%E3%83%86%E3%83%B3%E3%83%89%E3%83%BC%E3%82%AF%E3%83%A9%E3%82%B7%E3%83%83%E3%82%AF%E3%83%9F%E3%83%8B-%E3%83%95%E3%82%A1%E3%83%9F%E3%83%AA%E3%83%BC%E3%82%B3%E3%83%B3%E3%83%94%E3%83%A5%E3%83%BC%E3%82%BF-Amazon-co-jp%E9%99%90%E5%AE%9A-%E3%82%AA%E3%83%AA%E3%82%B8%E3%83%8A%E3%83%AB%E3%83%9D%E3%82%B9%E3%83%88%E3%82%AB%E3%83%BC%E3%83%89-30%E6%9E%9A%E3%82%BB%E3%83%83%E3%83%88/dp/B01LYBYK0L/";
 		String yodobashi = "http://www.yodobashi.com/%E4%BB%BB%E5%A4%A9%E5%A0%82-Nintendo-%E3%83%8B%E3%83%B3%E3%83%86%E3%83%B3%E3%83%89%E3%83%BC%E3%82%AF%E3%83%A9%E3%82%B7%E3%83%83%E3%82%AF%E3%83%9F%E3%83%8B-%E3%83%95%E3%82%A1%E3%83%9F%E3%83%AA%E3%83%BC%E3%82%B3%E3%83%B3%E3%83%94%E3%83%A5%E3%83%BC%E3%82%BF-%E3%82%B2%E3%83%BC%E3%83%A0%E6%A9%9F%E6%9C%AC%E4%BD%93/pd/100000001003263789/";
