7ac35da2d2df5b23f81f74a9a605115d39e83d28
hand-dot <kyouhei.fukuda0729@gmail.com>
2017-01-17 02:23:49 +0900

    ロギングとTODOの追加

--- src/main/java/hand_dot/famicon_mini/facade/StockCheckFacade.java
+++ src/main/java/hand_dot/famicon_mini/facade/StockCheckFacade.java
@@ -1,5 +1,7 @@
 package hand_dot.famicon_mini.facade;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import org.apache.log4j.Logger;
@@ -13,34 +15,41 @@ import hand_dot.famicon_mini.exception.SiteWatcherException;
 import hand_dot.famicon_mini.service.MailService;
 import hand_dot.famicon_mini.service.ProductService;
 import hand_dot.famicon_mini.service.SiteWatcherService;
+
 /**
  * サイトを監視し在庫のチェックを担当するファサードクラス
+ *
  * @author hand-dot
  *
  */
 @Component
 public class StockCheckFacade {
-	/** 商品サービス*/
-	 @Autowired
-	 ProductService productService;
+	/** 商品サービス */
+	@Autowired
+	ProductService productService;
 
-	/** サイト監視サービス*/
-	 @Autowired
-	 SiteWatcherService siteWatcherService;
+	/** サイト監視サービス */
+	@Autowired
+	SiteWatcherService siteWatcherService;
 
-	 /** メールサービス*/
-	 @Autowired
-	 MailService mailService;
-	 /**
-	  * 10秒間隔でサイトをチェックし在庫があればメールを送信します。
-	  */
-	 //TODO 実行時のログをとるようにする
+	/** メールサービス */
+	@Autowired
+	MailService mailService;
+
+	/**
+	 * 10秒間隔でサイトをチェックし在庫があればメールを送信します。
+	 */
+	//TODO ここをAOPでロギングするようにしたい。DBに実行結果を書き込めるようにしたい
 	@Scheduled(fixedRate = 10000)
 	public void stockCheck() {
 		/** ロガー */
 		Logger logger = Logger.getLogger("operation");
 		PropertyConfigurator.configure("log4j.properties");
-		List<ProductDto> productList =productService.getProductDtoListById("");
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		logger.info("在庫チェックを開始します。 - " + sdf.format(date));
+
+		List<ProductDto> productList = productService.getProductDtoListById("");
 		for (ProductDto product : productList) {
 			try {
 				siteWatcherService.setUrl(product.getUrl());
@@ -58,5 +67,8 @@ public class StockCheckFacade {
 				logger.error(e);
 			}
 		}
+
+		date = new Date();
+		logger.info("在庫チェックが終了しました。 - " + sdf.format(date));
 	}
 }
